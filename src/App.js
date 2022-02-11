93383453c445ccf50a29ac7255da12c0e7ba933e
Ric Park <ricpark@gmail.com>
2022-02-11 15:29:23 -0800

    Add image files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import logo from './logo.svg';
+import lv1 from './LV1.svg';
+import lv2 from './LV2.svg';
+import lv3 from './LV3.svg';
+import lv4 from './LV4.svg';
+import aceSpade from './ace-spade.png';
+import kingSpade from './king-spade.png';
+import queenSpade from './queen-spade.png';
+import jackSpade from './jack-spade.png';
+import tenSpade from './ten-spade.png';
+import theStar from './tarot-the-star.jpeg';
+
 import './App.css';
 import "nes.css/css/nes.min.css"
+import {useState} from "react";
+
+let logoHeightWidth = 65;
+const charmander = <i className="nes-charmander"></i>;
+const squirtle = <i className="nes-squirtle"></i>;
+const bulbasaur = <i className="nes-bulbasaur"></i>;
 
 function App() {
+  const [pokemon, setPokemon] = useState(<i className="nes-charmander"></i>);
+
+  function togglePokemon() {
+
+    if (pokemon === charmander) {
+      setPokemon(squirtle);
+    } else if (pokemon === squirtle) {
+      setPokemon(bulbasaur);
+    } else {
+      setPokemon(charmander);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+      <header className="App-page">
         <i className="nes-icon is-large trophy"> </i>
-        {<div className={"nes-container with-title"}>
-            <button className={"nes-btn is-warning"}>Louis Buitton</button>
-         </div>}
-        {/*<p>*/}
+        <img src={theStar}/>
+        <div>
+          <img src={tenSpade} className="App-logo" />
+          <img src={jackSpade} className="App-logo" />
+          <img src={queenSpade} className="App-logo"/>
+          <img src={kingSpade} className="App-logo" />
+          <img src={aceSpade} className="App-logo" />
+        </div>
+        <div>
+          <img src={lv1} height={logoHeightWidth} width={logoHeightWidth} className="App-logo" alt="logo" />
+          <img src={lv2} height={logoHeightWidth} width={logoHeightWidth} className="App-logo" alt="logo" />
+          <img src={lv3} height={logoHeightWidth} width={logoHeightWidth} className="App-logo" alt="logo" />
+          <img src={lv4} height={logoHeightWidth} width={logoHeightWidth} className="App-logo" alt="logo" />
+        </div>
+          <div className={"nes-container is-dark is-bordered"}>
+            {pokemon}
+          </div>
+          <button type="button" onClick={togglePokemon} className={"nes-btn is-warning"}>Louis Buitton</button>
+          {/*<p>*/}
         {/*  Edit <code>src/App.js</code> and save to reload.*/}
         {/*</p>*/}
         {/*<a*/}
